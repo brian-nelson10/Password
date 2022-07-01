71e4a34b43bbbb9e8c70c68f418bcbcefa44c45a
brian <bn3l10@gmail.com>
2022-06-30 23:22:58 -0400

    final edits

--- script.js
+++ script.js
@@ -17,27 +17,31 @@ var charType = prompt("Choose a character type: lowercase, uppercase, numeric, s
     window.confirm('You choose ' + charType + '.')
 
 
-//generate random password function
+//generate random password function 
 function generatePassword() {
-  //determine character type
   var charSet = "";
   var charTypeSet = charType.toLowerCase();
-  if( charTypeSet === "lowercase" ) {
+    if( charTypeSet === "lowercase" ) {
     charSet = "abcdefghijklmnopqrstuvwxyz";
+
   } else if( charTypeSet === "uppercase" ) {
     charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
   } else if( charTypeSet === "numeric" ) {
     charSet = "0123456789";
+
   } else if( charTypeSet === "special" ) {
-    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    charSet = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
   } 
-  //return value
-  var retVal = "";
-  for (var i = 0; i < length; i++) {
-    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+
+  //randomize values
+  var random = "";
+    for (var i = 0; i < length; i++) {
+    random += charSet.charAt(Math.floor(Math.random() * charSet.length));
   }
-  return retVal;
+    return random;
 }
+//random password
 window.confirm(generatePassword());
 document.getElementById('password').innerHTML=(generatePassword());
 
